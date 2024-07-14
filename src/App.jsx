ec0ebbb2aa333c09087f19b593941c389dc2e2a5
holgermueller <9jholger@gmail.com>
2024-07-14 15:27:43 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+     <h2>Content will go here.</h2>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
